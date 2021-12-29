9435eb33fcc7c3d76805df1d9d249187f732efd2
polo <polo>
2021-12-30 00:50:07 +0300

    Small refactoring was made.

--- Sources/IsEven/IsEven.swift
+++ Sources/IsEven/IsEven.swift
@@ -1,35 +1,23 @@
 import Foundation
+
 public struct IsEven {
-  //    public private(set) var text = "Hello, World!"
   
   private init() {
   }
   
   private static func parseJSON(_ data: Data) -> Bool? {
     let decoder = JSONDecoder()
-    if let decodedData = try? decoder.decode(IsEvenData.self, from: data) {
-      return decodedData.iseven
-    } else {
-      return nil
-    }
+    guard let decodedData = try? decoder.decode(IsEvenData.self, from: data) else { return nil }
+    
+    return decodedData.iseven
   }
   
   public static func isEven(_ number: Int) -> Bool? {
     let urlString = "https://api.isevenapi.xyz/api/iseven/\(number)/"
-    print(urlString)
+    guard let url = URL(string: urlString) else { return nil }
+    guard let data = try? Data(contentsOf: url) else { return nil }
+    guard let isEvenValue = parseJSON(data) else { return nil }
     
-    if let url = URL(string: urlString) {
-      if let data = try? Data(contentsOf: url) {
-        if let isEvenValue = parseJSON(data) {
-          return isEvenValue
-        } else {
-          return nil
-        }
-      } else {
-        return nil
-      }
-    } else {
-      return nil
-    }
+    return isEvenValue
   }
 }
